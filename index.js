27e19a09a8865282958014433f9bdc2c8662345a
raysonoon <raysonoonhere@gmail.com>
2025-08-13 20:31:05 +0800

    Connect frontend and configure CORS

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 // Custom morgan token to log request body for POST requests
 morgan.token('body', (req) => {
