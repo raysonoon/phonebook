af7867264fc434c5cebe8300cc39e02e6aa3dab9
raysonoon <raysonoonhere@gmail.com>
2025-08-13 21:04:17 +0800

    uibuild

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 
@@ -99,7 +100,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
